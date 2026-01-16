7e77e069633ae66cc6bde9984344f7a093251769
LHPH <luislyly@hotmail.com>
2026-01-15 19:49:36 -0600

    Adjust theme classes to check how it works

--- app/src/main/java/com/kyc/mobile/ui/theme/Type.kt
+++ app/src/main/java/com/kyc/mobile/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.kyc.mobile.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kyc.mobile.R
+
+val algerianFontFamily = FontFamily(
+    Font(R.font.algerian_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
